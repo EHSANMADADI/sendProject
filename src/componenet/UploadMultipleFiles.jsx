3e6707f77228b4d3b4926b530e2e692be77d64f2
Ehsanmadadi <ehsanmadadialengareqhi@gmail.com>
2024-08-22 11:05:40 +0330

    fix show mulyi photo

--- src/componenet/UploadMultipleFiles.jsx
+++ src/componenet/UploadMultipleFiles.jsx
@@ -1,4 +1,6 @@
+
 import React, { useState, useEffect } from 'react';
+import localforage from 'localforage';
 import axios from 'axios';
 import { CiCircleRemove } from "react-icons/ci";
 import { FaCheckCircle } from "react-icons/fa";
@@ -9,31 +11,26 @@ export default function UploadMultipleFiles({ keys, files, setSaveItems, saveIte
   const [progressAll, setProgressAll] = useState(0);
   const [fileStates, setFileStates] = useState(files.map(file => ({
     file,
-    // responseText: '',
+    responseText: '',
     src: '',
+    isSent: false,
+    progress: 0,
   })));
-  const [allFilesUploaded, setAllFilesUploaded] = useState(false);///اینو باید ببرم تو فایل مولتیپل و از طریق پراپز بفرستم اینجا
+  
+  const [allFilesUploaded, setAllFilesUploaded] = useState(false); 
   const [selectedImage, setSelectedImage] = useState();
   const handleUpload = (fileState, index) => {
     const formData = new FormData();
     formData.append('image', fileState.file);
-
-    //////create src for read image
-    // var src;
-    // const reader = new FileReader();
-    // reader.onload = (e) => {
-    //   setSelectedImage(e.target.result);
-    //   src = e.target.result
-    // }
-    // reader.readAsDataURL(fileState.file);
-    /////////////////////
-
-    const imageUrls = URL.createObjectURL(fileState.file);
-
-
-
-
-
+  
+    var imageUrls;
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setSelectedImage(e.target.result);
+      imageUrls = e.target.result;
+    };
+    reader.readAsDataURL(fileState.file);
+  
     axios.post('http://195.191.45.56:17010/process_image', formData, {
       onUploadProgress: (progressEvent) => {
         const percentage = Math.floor((progressEvent.loaded * 100) / progressEvent.total);
@@ -44,66 +41,61 @@ export default function UploadMultipleFiles({ keys, files, setSaveItems, saveIte
         });
       }
     })
-      .then(res => {
+      .then(async (res) => {
         console.log(res.data.pages[0].text);
-        setReseveDta(true);
-
+  
+        // Update the file state
         setFileStates(prevStates => {
           const updatedStates = [...prevStates];
           updatedStates[index].isSent = true;
-          // updatedStates[index].responseText = res.data.pages[0].text;
+          updatedStates[index].responseText = res.data.pages[0].text;
           updatedStates[index].src = imageUrls;
-          setProgressAll(100 / fileStates.length - index)
+          setProgressAll(100 / fileStates.length - index);
+  
           // Check if all files have been uploaded
           const allSent = updatedStates.every(state => state.isSent);
-
           if (allSent) {
             setAllFilesUploaded(true);
-            const newItem = JSON.stringify(fileStates)
-            const storedArray = JSON.parse(sessionStorage.getItem('multiSeavedItems')) || [];
-            const updatedArray = [newItem, ...storedArray];
-            setSaveItems([updatedArray]);
+            console.log(fileStates);
+  
+            // Manage the localforage storage
+            localforage.getItem('multiSeavedItems').then(async (storedArray) => {
+              const parsedArray = storedArray ? JSON.parse(storedArray) : [];
+              const updatedArray = [updatedStates, ...parsedArray];
+              setSaveItems(updatedArray);
+            });
           }
+  
           return updatedStates;
         });
-        console.log(allFilesUploaded);
-
       })
       .catch(err => {
         alert(`فایل ${fileState.file.name} ارسال نشد`);
         console.log(err);
       });
   };
-  useEffect(() => {
-    if (files) {
-      setFileStates(files.map(file => ({
-        file,
-        src: '',
-      })));
-    }
-  }, [files]);
+  
+
 
   useEffect(() => {
-    setReseveDta(false);
+   
     fileStates.forEach((fileState, index) => {
       if (!fileState.isSent) {
         handleUpload(fileState, index);
       }
     });
-
-
   }, []);
 
   return (
     <div>
       {
-        saveItems.length === 0 && <div className='flex text-center justify-center text-gray-500 text-2xl font-bold mt-10'>
+        saveItems.length == 0&& !files && <div className='flex text-center justify-center text-gray-500 text-2xl font-bold mt-10'>
           <p className='text-center'> فایلی موجود نیست لطفا فایلی را انتخاب نمایید</p>
         </div>
       }
 
       {
-        !allFilesUploaded && !reseveDta && <div className='w-full'>
+        !allFilesUploaded  && <div className='w-full'>
           <div className='border border-gray-100 shadow-2xl rounded-lg  xl:mx-6 mx-1 xl:p-5 py-2 mb-10'>
             <div className='flex justify-between items-center md:mx-5 mx-2'>
               <p className='text-xl font-semibold overflow-clip'>درحال پردازش لطفا صبر کنید</p>
--- src/pages/Multipel.jsx
+++ src/pages/Multipel.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import localforage from 'localforage';
 import Modal from '../componenet/Modal';
 import img from '../images/img.png'
 import InputMultiple from '../componenet/InputMultiple';
@@ -9,31 +9,44 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import useStore from '../Store/store.ts';
 import { FaCheckCircle } from "react-icons/fa"
 import UploadMultipleFiles from '../componenet/UploadMultipleFiles.jsx';
+
 export default function Multipel() {
     const [files, setFiles] = useState(null);
     const [error, setError] = useState('');
-
     const [openModals, setOpenModals] = useState([]);
-    const [saveItems, setSaveItems] = useState(() => {
-        const storage = JSON.parse(sessionStorage.getItem('multiSeavedItems')) || [];
-        return storage;
-    });
-    // console.log(saveItem);
+    const [saveItems, setSaveItems] = useState([]);
+
+    useEffect(() => {
+        async function getSavedItems() {
+            const storedItems = await localforage.getItem('multiSeavedItems');
+            setSaveItems(JSON.parse(storedItems) || []);
+        }
+        getSavedItems();
+    }, []);
 
     const { setShowBTN } = useStore();
 
     useEffect(() => {
-        sessionStorage.setItem('multiSeavedItems', JSON.stringify(saveItems));
+        if (saveItems.length > 0) {
+            localforage.setItem('multiSeavedItems', JSON.stringify(saveItems));
+        }
     }, [saveItems]);
 
     const handelremove = (id) => {
         const updatedItems = saveItems.filter((_, i) => i !== id);
         setSaveItems(updatedItems);
-        Swal.fire({
-            title: "فایل با موفقیت حذف شد",
-            icon: "success"
+    
+        // به‌روزرسانی مقادیر در localforage
+        localforage.setItem('multiSeavedItems', JSON.stringify(updatedItems)).then(() => {
+            Swal.fire({
+                title: "فایل با موفقیت حذف شد",
+                icon: "success"
+            });
+        }).catch((error) => {
+            console.error("Error updating localforage: ", error);
         });
     };
+    
 
     const handleModalOpen = (index, txt) => {
         localStorage.setItem('txt', txt)
@@ -48,112 +61,90 @@ export default function Multipel() {
         setOpenModals(updatedOpenModals);
     };
 
-    
-
-
     return (
         <div className='flex lg:overflow-hidden lg:flex-nowrap flex-wrap lg:h-screen '>
-            <div className='w-2/3  mx-auto'>
+            <div className='w-2/3 mx-auto'>
                 <InputMultiple files={files} setFiles={setFiles} error={error} setError={setError} />
             </div>
-            <div className='h-screen  lg:w-1/2 w-full mx-auto  sm:mr-20'>
+            <div className='h-screen lg:w-1/2 w-full mx-auto sm:mr-20'>
                 <div>
                     <img className='md:w-1/2 w-0 mx-auto ' src={img} alt='img-banner' />
                 </div>
                 <div className='w-full md:h-1/2 h-full overflow-auto scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full  scrollbar-thumb-yellow-600 scrollbar-track-gray-50 '>
                     {
-                        !files && !saveItems.length && <div className='flex  justify-center text-gray-500 sm:text-2xl text-base font-bold mt-10 text-center'>
-                            <p> فایلی موجود نیست لطفا فایلی را انتخاب نمایید</p>
+                        !files && saveItems.length === 0 && <div className='flex justify-center text-gray-500 sm:text-2xl text-base font-bold mt-10 text-center'>
+                            <p>فایلی موجود نیست لطفا فایلی را انتخاب نمایید</p>
                         </div>
                     }
                     {
-                        files  && <UploadMultipleFiles    key={files ? files.length : saveItems.length} files={files} setFiles={setFiles} setSaveItems={setSaveItems} saveItems={saveItems} />
+                        files && <UploadMultipleFiles files={files} setFiles={setFiles} setSaveItems={setSaveItems} saveItems={saveItems} />
                     }
 
                     {
-                       saveItems.length > 0 && saveItems.map((item, index) => {
-                           
-
-                            return (
-                                <div key={index} className='seavItem border border-gray-100 shadow-2xl rounded-lg xl:mx-6 mx-1 xl:p-5 py-2 mb-10 aaaaaaaaaaaa'>
-                                    <div className='flex justify-between items-center md:mx-5 mx-2'>
-                                        <p className='text-xl font-semibold'>پردازش تکمیل شد</p>
-                                        <div className='text-lg text-green-500'>
-                                            <FaCheckCircle />
+                        saveItems.length > 0 && saveItems.map((itemArray, index) => (
+                            <div key={index} className='seavItem border border-gray-100 shadow-2xl rounded-lg xl:mx-6 mx-1 xl:p-5 py-2 mb-10'>
+                                <div className='flex justify-between items-center md:mx-5 mx-2'>
+                                    <p className='text-xl font-semibold'>پردازش تکمیل شد</p>
+                                    <div className='text-lg text-green-500'>
+                                        <FaCheckCircle />
+                                    </div>
+                                </div>
+                                <div className='sm:mx-6 mx-0 mt-1'>
+                                    <div className="bg-gray-200 rounded-full h-2 dark:bg-gray-700 ">
+                                        <div className="bg-orange-400 h-2 rounded-full" style={{ width: '100%' }}></div>
+                                    </div>
+                                    <div className="flex justify-end mb-1">
+                                        <span className="text-sm font-medium text-gray-400 dark:text-white">100%</span>
+                                    </div>
+                                    <div className='flex justify-between'>
+                                        <div className='flex'>
+                                            <button className='border-dotted border-black rounded-md border-2 px-4 pt-1 pb-2 mx-2 sm:text-xl text-xs font-semibold text-center flex items-center hover:scale-105 duration-200' onClick={() => { setShowBTN(true); handleModalOpen(index, itemArray.txt) }}><span className='text-center mt-2 mr-2 text-2xl '><IoMdEye /></span>نمایش </button>
+                                            <button className='border-dotted border-black rounded-md border-2 px-4 pt-1 pb-2 mx-2 sm:text-xl text-xs font-semibold text-center flex items-center hover:scale-105 duration-200' onClick={() => handelremove(index)}><span className='text-center mt-2 mr-2 text-2xl text-red-600 '><RiDeleteBin6Line /></span>حذف </button>
                                         </div>
                                     </div>
-                                    <div className='sm:mx-6 mx-0 mt-1'>
-                                        <div className="bg-gray-200 rounded-full h-2 dark:bg-gray-700 ">
-                                            <div className="bg-orange-400 h-2 rounded-full" style={{ width: '100%' }}></div>
-                                        </div>
-                                        <div className="flex justify-end mb-1">
-                                            <span className="text-sm font-medium text-gray-400 dark:text-white">100%</span>
-                                        </div>
-                                        <div className='flex justify-between'>
-                                            <div className='flex'>
-                                                <button className='border-dotted border-black rounded-md border-2 px-4 pt-1 pb-2 mx-2 sm:text-xl text-xs font-semibold text-center flex items-center hover:scale-105 duration-200' onClick={() => { setShowBTN(true); handleModalOpen(index, item.txt) }}><span className='text-center mt-2 mr-2 text-2xl '><IoMdEye /></span>نمایش </button>
-                                                <button className='border-dotted border-black rounded-md border-2 px-4 pt-1 pb-2 mx-2 sm:text-xl text-xs font-semibold text-center flex items-center  hover:scale-105 duration-200' onClick={() => handelremove(index)}><span className='text-center mt-2 mr-2 text-2xl text-red-600 ' onClick={() => { handelremove() }}><RiDeleteBin6Line /></span>حذف </button>
-                                            </div>
-                                        </div>
-                                    </div>
+                                </div>
 
-                                    {
-                                        item.map((file, i) => {
-                                           
-                                            
-                                            try {
-                                                const ArrayDetaiels = JSON.parse(item);
-                                                console.log(typeof(ArrayDetaiels));
-                                               console.log(ArrayDetaiels);
-                                               
-                                                return (
-                                                    <Modal
-                                                        key={i}
-                                                        Open={openModals[index]}
-                                                        onClose={() => handleModalClose(index)}
+                                {
+                                    itemArray.map((file, i) => (
+                                        <Modal
+                                            key={i}
+                                            Open={openModals[index]}
+                                            onClose={() => handleModalClose(index)}
+                                        >
+                                            <div key={i} className="flex h-full">
+                                                <div dir='rtl' className="w-1/2 overflow-x-auto max-h-[80vh] p-2">
+                                                    <div className={`grid grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-${Math.ceil(itemArray.length / 2)}`}>
+                                                        {itemArray.map((detail, index) => (
+                                                            <div key={index} className="relative">
+                                                                <img
+                                                                    className="w-full h-auto  object-cover rounded-lg"
+                                                                    src={detail.src}
+                                                                    alt={`detail-${index}`}
+                                                                />
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                </div>
 
-                                                    >
-                                                        <div key={i} className="flex h-full">
-
-                                                            <div className="w-1/2 overflow-x-auto p-2">
-                                                                <div className={`grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-[${ArrayDetaiels.length / 2}]`}>
-                                                                    {ArrayDetaiels.map((detail, index) => (
-                                                                        <div key={index} className="relative">
-                                                                            <img
-                                                                                className="w-full h-auto object-cover rounded-lg"
-                                                                                src={detail.src}
-                                                                                alt={`detail-${index}`}
-                                                                            />
-                                                                        </div>
-                                                                    ))}
+                                                <div dir="rtl" className="w-1/2 p-4 bg-gray-50 overflow-auto max-h-[80vh]">
+                                                    <pre className="text-sm font-black">
+                                                        {itemArray.map((detail, index) => (
+                                                            <div key={index} className="border-b-[3px] border-dashed pb-2">
+                                                                {detail.responseText}
+                                                                <div className="text-left text-sm text-gray-600 mt-1">
+                                                                    {`شماره: ${index + 1}`}
                                                                 </div>
                                                             </div>
-
-                                                            {/* بخش متن */}
-                                                            {/* <div dir='rtl' className="w-1/2 p-4 bg-gray-50 overflow-auto max-h-[700px]">
-                                                                <pre className="text-sm font-black border-t-2">{ArrayDetaiels.map(detail => detail.responseText).join("\n\n")}</pre>
-                                                            </div> */}
-                                                        </div>
-                                                    </Modal>
-
-                                                );
-                                            } catch (error) {
-                                                console.error('Error parsing JSON:', error, 'at item index:', i);
-                                                return null; // در صورتی که JSON به درستی پارس نشود، می‌توانید مقدار null را بازگردانید.
-                                            }
-                                        })
-                                    }
-
-
-
-
-                                </div>
-                            )
-                        }
-                        )
+                                                        ))}
+                                                    </pre>
+                                                </div>
+                                            </div>
+                                        </Modal>
+                                    ))
+                                }
+                            </div>
+                        ))
                     }
-
-
                 </div>
             </div>
         </div>
